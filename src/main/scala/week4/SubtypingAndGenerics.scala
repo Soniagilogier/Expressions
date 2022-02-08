950cff1561430e856f1496f9303fbaf98d825883
Sònia <98806800+Soniagilogier@users.noreply.github.com>
2022-02-08 16:46:42 +0100

    new classes

--- src/main/scala/week4/SubtypingAndGenerics.scala
+++ src/main/scala/week4/SubtypingAndGenerics.scala
@@ -7,5 +7,13 @@ object SubtypingAndGenerics {
 
   def assertAllPos[S <: IntSet] (r: S): S = ???
 
+  /**
+   * In the method defined above we can see "[S < : IntSet]" which means that S is subtype of IntSet
+   * If, instead, we had "[S >: NonEmpty < : IntSet]" it would mean that S is supertype of NonEmpty and subtype of IntSet
+   *
+   * The Barbara Liskov's substitution principle (LSP) states that 'A' is a subtype of 'B' if anything that one can do
+   * with a value of type 'B' one can also do with a value of type 'A'
+   *
+   */
 
 }
